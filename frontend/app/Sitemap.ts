26309bfad1bb83d323f4de1144a34ba22895aa32
khazrytech <99062625+khazrytech@users.noreply.github.com>
2026-06-28 04:38:56 +0300

    Add sitemap function for MetadataRoute

--- frontend/app/Sitemap.ts
+++ frontend/app/Sitemap.ts
@@ -1,1 +1,11 @@
 
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://kadotv.work.gd',
+      lastModified: new Date(),
+    },
+  ]
+}
