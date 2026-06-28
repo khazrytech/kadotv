9e6925c0fec8998e5c1ffe08f03c521d3004ddfd
khazrytech <99062625+khazrytech@users.noreply.github.com>
2026-06-28 04:51:37 +0300

    Update sitemap to include login page
    Added login page to sitemap with monthly change frequency.

--- frontend/app/Sitemap.ts
+++ frontend/app/Sitemap.ts
@@ -1,11 +1,19 @@
-
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://kadotv.work.gd',
+      url: 'https://kadotv.work.gd', // Hii ni homepage
       lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    // Ongeza kurasa zingine hapa bila kurudia ile ya juu
+    {
+      url: 'https://kadotv.work.gd/login', 
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
   ]
 }
